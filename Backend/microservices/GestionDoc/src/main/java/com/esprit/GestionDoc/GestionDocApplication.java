2a0e325d27935bdba7f4120043f7ddf1d99c18cf
karimarbi <karim.arbi007@outlook.fr>
2025-02-18 02:15:42 +0100

    push GestionDoc

---
+++ Backend/microservices/GestionDoc/src/main/java/com/esprit/GestionDoc/GestionDocApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.GestionDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDocApplication.class, args);
+	}
+
+}
---
+++ Backend/microservices/GestionDoc/src/test/java/com/esprit/GestionDoc/GestionDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.GestionDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
